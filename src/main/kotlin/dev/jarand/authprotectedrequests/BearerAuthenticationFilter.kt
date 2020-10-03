42d4cecc537d0351a2ce648cc75abfe23a23f686
JarandDev <jarand@jarand.dev>
2020-10-03 22:26:19 +0200

    #5: Trying to fix the refresh token flow

--- src/main/kotlin/dev/jarand/authprotectedrequests/BearerAuthenticationFilter.kt
+++ src/main/kotlin/dev/jarand/authprotectedrequests/BearerAuthenticationFilter.kt
@@ -3,12 +3,14 @@ package dev.jarand.authprotectedrequests
 import dev.jarand.authprotectedrequests.authapi.AuthApiClient
 import dev.jarand.authprotectedrequests.jws.JwsService
 import dev.jarand.authprotectedrequests.jws.ParseClaimsResultState
+import io.jsonwebtoken.Claims
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import java.util.*
+import java.util.stream.Collectors
 import javax.servlet.Filter
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
@@ -25,64 +27,68 @@ class BearerAuthenticationFilter(private val jwsService: JwsService, private val
         val response = servletResponse as HttpServletResponse
 
         if (request.cookies == null || request.cookies.isEmpty()) {
-            logger.debug("No cookies. Returning 401.")
+            logger.debug("No cookies sent with request. Returning 401.")
             response.sendError(HttpStatus.UNAUTHORIZED.value())
             return
         }
 
-        val accessTokenCookie = Arrays.stream(request.cookies).filter { it.name == "access_token" }.findFirst().orElse(null)
-        val refreshTokenCookie = Arrays.stream(request.cookies).filter { it.name == "refresh_token" }.findFirst().orElse(null)
+        val tokenCookies = Arrays.stream(request.cookies)
+                .filter { it.name == "access_token" || it.name == "refresh_token" }
+                .collect(Collectors.toUnmodifiableList())
+        logger.debug("Found ${tokenCookies.size} token cookies.")
 
-        if (accessTokenCookie == null && refreshTokenCookie == null) {
-            logger.debug("No access or refresh token in cookies. Returning 401.")
-            response.sendError(HttpStatus.UNAUTHORIZED.value())
-            return
-        }
-
-        var accessToken: String? = null
-        if (accessTokenCookie != null) {
-            accessToken = accessTokenCookie.value
-            var result = jwsService.parseClaims(accessToken)
-            if (result.state == ParseClaimsResultState.EXPIRED && refreshTokenCookie != null) {
-                val refreshedAccessToken = authApiClient.refreshToken(refreshTokenCookie.value)
-                refreshedAccessToken?.let {
-                    result = jwsService.parseClaims(it)
-                    response.addCookie(createCookie(it, accessTokenCookie))
-                    accessToken = it
+        for (tokenCookie in tokenCookies) {
+            if (tokenCookie.name == "access_token") {
+                logger.debug("Processing access_token.")
+                val result = jwsService.parseClaims(tokenCookie.value)
+                if (result.state == ParseClaimsResultState.SUCCESS && result.claims != null) {
+                    logger.debug("Successfully parsed access_token. Setting security context.")
+                    setSecurityContext(result.claims)
+                } else if (result.state != ParseClaimsResultState.EXPIRED) {
+                    logger.debug("Parse failed with state ${result.state}. Returning 401.")
+                    response.sendError(HttpStatus.UNAUTHORIZED.value())
+                    return
+                }
+            } else if (tokenCookie.name == "refresh_token") {
+                logger.debug("Processing refresh_token.")
+                val accessToken = authApiClient.refreshToken(tokenCookie.value)
+                if (accessToken == null) {
+                    logger.debug("No access_token returned when trying to refresh token. Returning 401.")
+                    response.sendError(HttpStatus.UNAUTHORIZED.value())
+                    return
+                }
+                val result = jwsService.parseClaims(tokenCookie.value)
+                if (result.state == ParseClaimsResultState.SUCCESS && result.claims != null) {
+                    logger.debug("Successfully parsed access_token after refreshing. Adding new access token to cookie.")
+                    response.addCookie(createCookie(accessToken, tokenCookie))
+                    logger.debug("Successfully added access_token to cookie. Setting security context.")
+                    setSecurityContext(result.claims)
+                } else if (result.state != ParseClaimsResultState.EXPIRED) {
+                    logger.debug("Parse after refreshing failed with state ${result.state}. Returning 401.")
+                    response.sendError(HttpStatus.UNAUTHORIZED.value())
+                    return
                 }
             }
         }
 
-        if (accessToken == null) {
-            logger.debug("No access token potentially refreshing expired token. Returning 401.")
-            response.sendError(HttpStatus.UNAUTHORIZED.value())
-            return
-        }
+        chain.doFilter(request, response)
+    }
 
-        val result = jwsService.parseClaims(accessToken!!)
-        if (result.state != ParseClaimsResultState.SUCCESS) {
-            logger.debug("Could not parse access token. State: ${result.state}. Returning 401.")
-            response.sendError(HttpStatus.UNAUTHORIZED.value())
-            return
-        }
-
-        val claims = result.claims!!
+    private fun setSecurityContext(claims: Claims) {
         val authorities = claims.get("scope", String::class.java).split(" ").stream().map { SimpleGrantedAuthority("ROLE_$it") }.toList()
         val authentication = UsernamePasswordAuthenticationToken(claims.subject, null, authorities)
 
         val securityContext = SecurityContextHolder.getContext()
         securityContext.authentication = authentication
-
-        chain.doFilter(request, response)
     }
 
-    private fun createCookie(accessToken: String, oldAccessTokenCookie: Cookie): Cookie {
+    private fun createCookie(accessToken: String, refreshTokenCookie: Cookie): Cookie {
         val cookie = Cookie("access_token", accessToken)
-        cookie.isHttpOnly = oldAccessTokenCookie.isHttpOnly
-        cookie.secure = oldAccessTokenCookie.secure
-        cookie.domain = oldAccessTokenCookie.domain
-        cookie.path = oldAccessTokenCookie.path
-        cookie.maxAge = oldAccessTokenCookie.maxAge
+        cookie.isHttpOnly = refreshTokenCookie.isHttpOnly
+        cookie.secure = refreshTokenCookie.secure
+        cookie.domain = refreshTokenCookie.domain
+        cookie.path = refreshTokenCookie.path
+        cookie.maxAge = 3600
         return cookie
     }
 
